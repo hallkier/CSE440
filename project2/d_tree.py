4b19efec27245eef1b04df6684965c49cc180f0f
hallkier <hallkier@msu.edu>
2016-12-03 17:56:46 -0500

    update spacing

--- project2/d_tree.py
+++ project2/d_tree.py
@@ -4,10 +4,11 @@ def DT(filename):
        num_inst = 0
        data_bool = false
        for line in f:
+           if line == ("@data"):
+                data_bool = true
+
            if data_bool == true:
                num_inst = num_inst + 1
-            if line == ("@data"):
-                data_bool = true
 
         print ("Number of instances: ", num_inst)
 
