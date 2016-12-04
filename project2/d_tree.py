8bc1b28c0180284edd765d1e59928b967b41900e
Kieran Hall <hallkier@msu.edu>
2016-12-04 14:49:31 -0500

    update file

--- project2/d_tree.py
+++ project2/d_tree.py
@@ -2,12 +2,12 @@ def DT(filename):
 
    with open(filename, 'r') as f:
        num_inst = 0
-       data_bool = false
+       data_bool = False
        for line in f:
            if line == ("@data"):
-                data_bool = true
+                data_bool = True
 
-           if data_bool == true:
+           if data_bool == True:
                num_inst = num_inst + 1
        print ("Number of instances: ", num_inst)
 
